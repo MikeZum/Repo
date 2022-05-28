8a0167b2d050a76c37c9525246c43a13721271cd
«Mike» <michail.v.tolmachev@gmail.com>
2022-05-28 18:10:34 +0800

    Домашка1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-lesson011;
+let title = "mySales";
+let screens = "myScreens";
+let screenPrice = 1000;
+let rollback = 20;
+let fullPrice = 5000;
+let adaptive = "myAdaptive";
+
+alert("Hi!");
+
+console.log("myFirstMessege");
