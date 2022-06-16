53cb0ee9667932a53c7a1aa2de22cd17340e5215
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-16 13:35:29 +0800

    lesson15

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 "use strict";
 
-const btn = document.querySelector("#btn");
-const text = document.querySelector("#text");
-const eBtn = document.querySelector("#e_btn");
-const range = document.querySelector("#range");
-const circle = document.querySelector("#circle");
-const inputText = document.querySelector("#text");
-let textColor;
+const body = document.querySelector("body");
+const cssText =
+  "height: 100px; width: 100px; background - color: yellow; font - size: 14px;";
 
-const getColor = function () {
-  textColor = inputText.value;
+const DomElement = function (selector) {
+  this.selector = selector;
+  this.height = "100px";
+  this.width = "150px";
+  this.bg = "yellow";
+  this.fontSize = "20px";
 };
 
-const changeColor = function () {
-  square.style.backgroundColor = textColor;
-  inputText.value = "";
+DomElement.prototype.createElement = function () {
+  if (this.selector[0] == ".") {
+    const addClass = this.selector.slice(1);
+    const newDiv = document.createElement("div");
+    newDiv.className = addClass;
+    newDiv.textContent = "Div";
+    newDiv.style.cssText = `height: ${this.height}; width: ${this.width}; 
+        background: ${this.bg}; font-size: ${this.fontSize};`;
+    document.body.append(newDiv);
+  } else if (this.selector[0] == "#") {
+    const addId = this.selector.slice(1);
+    const newP = document.createElement("p");
+    newP.id = addId;
+    newP.textContent = "P";
+    newP.style.cssText = `height: ${this.height}; width: ${this.width}; 
+        background: ${this.bg}; font-size: ${this.fontSize};`;
+    document.body.append(newP);
+  }
 };
 
-eBtn.style.display = "none";
+const elementClass = new DomElement(".block");
+const elementId = new DomElement("#best");
 
-range.addEventListener("input", () => {
-  circle.style.height = range.value + "%";
-  circle.style.width = range.value + "%";
-});
-
-inputText.addEventListener("input", getColor);
-btn.addEventListener("click", changeColor);
+elementClass.createElement();
+elementId.createElement();
