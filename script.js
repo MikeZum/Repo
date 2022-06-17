ca90acf25092ba358b3563807aaab035cbac4d4b
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-17 11:20:44 +0800

    lesson16

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
 "use strict";
 
-const body = document.querySelector("body");
-const cssText =
-  "height: 100px; width: 100px; background - color: yellow; font - size: 14px;";
-
-const DomElement = function (selector) {
-  this.selector = selector;
-  this.height = "100px";
-  this.width = "150px";
-  this.bg = "yellow";
-  this.fontSize = "20px";
-};
-
-DomElement.prototype.createElement = function () {
-  if (this.selector[0] == ".") {
-    const addClass = this.selector.slice(1);
-    const newDiv = document.createElement("div");
-    newDiv.className = addClass;
-    newDiv.textContent = "Div";
-    newDiv.style.cssText = `height: ${this.height}; width: ${this.width}; 
-        background: ${this.bg}; font-size: ${this.fontSize};`;
-    document.body.append(newDiv);
-  } else if (this.selector[0] == "#") {
-    const addId = this.selector.slice(1);
-    const newP = document.createElement("p");
-    newP.id = addId;
-    newP.textContent = "P";
-    newP.style.cssText = `height: ${this.height}; width: ${this.width}; 
-        background: ${this.bg}; font-size: ${this.fontSize};`;
-    document.body.append(newP);
+class First {
+  hello() {
+    console.log("Привет я метод родителя!");
   }
-};
+}
 
-const elementClass = new DomElement(".block");
-const elementId = new DomElement("#best");
+class Second extends First {
+  constructor(hello) {
+    super(hello);
+  }
 
-elementClass.createElement();
-elementId.createElement();
+  hello() {
+    console.log("А я наследуемый метод!");
+  }
+}
+
+const first = new First();
+first.hello();
+
+const second = new Second(first.hello());
+// first.hello();
+second.hello();
